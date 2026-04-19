d5585551a5a3f77e41f2e900443c659ad884fd1f
Sergey Zyukin <zyukin.s@icloud.com>
2026-04-19 17:39:16 +0300

    Update CS_RT_BIN_PERM_RAISE detector to handle chmod kprobes and expand directory scope

--- event-processor/detector/wasm/CS_RT_BIN_PERM_RAISE/main.go
+++ event-processor/detector/wasm/CS_RT_BIN_PERM_RAISE/main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"regexp"
 
 	"github.com/gobwas/glob"
@@ -13,9 +14,9 @@ import (
 
 const (
 	ID          = "CS_RT_BIN_PERM_RAISE"
-	Name        = "Change of file access permissions"
-	Description = "The detector detects if the chmod command was started. The command changes file access permissions in the home, tmp, var, boot, media, mnt, dev, run, and sys directories by adding a permission to a specific user, group, or all users."
-	Version     = 2
+	Name        = "Change of file access permisions"
+	Description = "The detector detects if permissions to execute files in the boot, dev, home, media, mnt, run, sys, tmp, and var directories were granted."
+	Version     = 3
 	Author      = "Runtime Radar Team"
 	License     = "Apache License 2.0"
 )
@@ -26,7 +27,8 @@ var (
 	// a particular event type, such as "PROCESS_EXEC", leave slice empty or use
 	// wildcard "*".
 	triggerCriteria = map[string][]string{
-		"PROCESS_EXEC": {},
+		"PROCESS_EXEC":   {},
+		"PROCESS_KPROBE": {"security_path_chmod"},
 
 		// Examples:
 		//
@@ -41,20 +43,20 @@ var (
 var (
 	chmod       = glob.MustCompile(`*/chmod`)
 	execPerm    = regexp.MustCompile(`^(?:[\-Rcfv\s]+)?(?:(?:[0-7]?[0-7][1357][0-7]\s|[0-7]?[1357][0-7][0-7]\s|[0-7]?[0-7][0-7][1357]\s)|(?:[ugoa]*[+=]x\s))`)
-	suspDirArgs = regexp.MustCompile(`^(?:[\-Rcfv\s]+)?(?:(?:[ugoa]*[-=+][rwxX])|(?:[0-7]{3,4}))(?:.*)\/?(?:home|tmp|var|boot|media|mnt|dev|run|sys)\/`)
+	suspDirArgs = regexp.MustCompile(`^(?:[\-Rcfv\s]+)?(?:(?:[ugoa]*[-=+][rwxX])|(?:[0-7]{3,4}))(?:.*)\/?(?:boot|dev|home|media|mnt|run|sys|tmp|var)\/`)
 	relPath     = regexp.MustCompile(`^(?:[\-Rcfv\s]+)?(?:(?:[ugoa]*[-=+][rwxX])|(?:[0-7]{3,4}))(?:.*)\s(?:[^\/])`)
 
 	suspDirCwd = []glob.Glob{
-		glob.MustCompile("/home*"),
-		glob.MustCompile("/tmp*"),
-		glob.MustCompile("/var*"),
 		glob.MustCompile("/boot*"),
+		glob.MustCompile("/dev*"),
+		glob.MustCompile("/home*"),
 		glob.MustCompile("/media*"),
 		glob.MustCompile("/mnt*"),
+		glob.MustCompile("/run*"),
 		glob.MustCompile("/srv*"),
 		glob.MustCompile("/sys*"),
-		glob.MustCompile("/dev*"),
-		glob.MustCompile("/run*"),
+		glob.MustCompile("/tmp*"),
+		glob.MustCompile("/var*"),
 	}
 )
 
@@ -152,7 +154,32 @@ func (d Detector) Detect(ctx context.Context, req *api.DetectReq) (*api.DetectRe
 	case *tetragon.GetEventsResponse_ProcessExit:
 		// Nothing here
 	case *tetragon.GetEventsResponse_ProcessKprobe:
-		// Nothing here
+		kprobe := ev.ProcessKprobe
+		function := kprobe.GetFunctionName()
+		args := kprobe.GetArgs()
+		path := ""
+
+		switch function {
+		case "security_path_chmod":
+			if len(args) < 2 {
+				return nil, fmt.Errorf("unexpected args len, got %d, want >= 2", len(args))
+			}
+
+			path = args[0].GetPathArg().GetPath()
+		default:
+			return resp, nil
+		}
+
+		for _, dir := range suspDirCwd {
+			if dir.Match(path) {
+				resp.Severity = api.DetectResp_MEDIUM // <-- threat detected
+
+				return resp, nil
+			}
+		}
+
+		return resp, nil
+
 	case *tetragon.GetEventsResponse_ProcessTracepoint:
 		// Nothing here
 	}
@@ -161,6 +188,9 @@ func (d Detector) Detect(ctx context.Context, req *api.DetectReq) (*api.DetectRe
 }
 
 /* Example event (JSON):
+
+PROCESS_EXEC
+
 {
     "process_exec": {
         "process": {
@@ -415,8 +445,314 @@ func (d Detector) Detect(ctx context.Context, req *api.DetectReq) (*api.DetectRe
         },
         "ancestors": []
     },
-    "node_name": "ptcs-master-node",
+    "node_name": "experts-k8s-cs",
     "time": "2024-11-13T10:15:07.462452981Z",
     "aggregation_info": null
 }
+
+PROCESS_KPROBE
+
+{
+    "process_kprobe": {
+        "process": {
+            "exec_id": "dnNoLWs4cy5hcHBzZWMtc3RhbmQucHRzZWN1cml0eS5jbG91ZDoxODEwOTkyMTM4Njc5NjQ3NjoyNzQ1MTE3",
+            "pid": 2745117,
+            "uid": 0,
+            "cwd": "/tmp",
+            "binary": "/usr/bin/chmod",
+            "arguments": "o+x reg-file",
+            "flags": "execve clone",
+            "start_time": "2025-12-10T19:03:39.717065187Z",
+            "auid": 4294967295,
+            "pod": {
+                "namespace": "default",
+                "name": "test-pod-debian",
+                "container": {
+                    "id": "containerd://35ae39802175a6d328218c2d35d878bf0402b37a229ee28b2960161c567eca5d",
+                    "name": "test-pod-debian",
+                    "image": {
+                        "id": "docker.io/library/debian@sha256:7e5bc0e499a8d50cb1e32287944a90b9ec8fd7d500673e75daff3f52882f5798",
+                        "name": "docker.io/library/debian:12"
+                    },
+                    "start_time": "2025-12-10T07:50:30Z",
+                    "pid": 174,
+                    "maybe_exec_probe": false,
+                    "security_context": {
+                        "privileged": false
+                    }
+                },
+                "pod_labels": {},
+                "workload": "test-pod-debian",
+                "workload_kind": "Pod",
+                "pod_annotations": {}
+            },
+            "docker": "35ae39802175a6d328218c2d35d878b",
+            "parent_exec_id": "dnNoLWs4cy5hcHBzZWMtc3RhbmQucHRzZWN1cml0eS5jbG91ZDoxODEwNzg5OTk2NDM0Nzg5MDoyNzAxOTgy",
+            "refcnt": 1,
+            "cap": {
+                "permitted": [
+                    "CAP_CHOWN",
+                    "DAC_OVERRIDE",
+                    "CAP_FOWNER",
+                    "CAP_FSETID",
+                    "CAP_KILL",
+                    "CAP_SETGID",
+                    "CAP_SETUID",
+                    "CAP_SETPCAP",
+                    "CAP_NET_BIND_SERVICE",
+                    "CAP_NET_RAW",
+                    "CAP_SYS_CHROOT",
+                    "CAP_MKNOD",
+                    "CAP_AUDIT_WRITE",
+                    "CAP_SETFCAP"
+                ],
+                "effective": [
+                    "CAP_CHOWN",
+                    "DAC_OVERRIDE",
+                    "CAP_FOWNER",
+                    "CAP_FSETID",
+                    "CAP_KILL",
+                    "CAP_SETGID",
+                    "CAP_SETUID",
+                    "CAP_SETPCAP",
+                    "CAP_NET_BIND_SERVICE",
+                    "CAP_NET_RAW",
+                    "CAP_SYS_CHROOT",
+                    "CAP_MKNOD",
+                    "CAP_AUDIT_WRITE",
+                    "CAP_SETFCAP"
+                ],
+                "inheritable": []
+            },
+            "ns": {
+                "uts": {
+                    "inum": 4026533802,
+                    "is_host": false
+                },
+                "ipc": {
+                    "inum": 4026533803,
+                    "is_host": false
+                },
+                "mnt": {
+                    "inum": 4026533805,
+                    "is_host": false
+                },
+                "pid": {
+                    "inum": 4026533806,
+                    "is_host": false
+                },
+                "pid_for_children": {
+                    "inum": 4026533806,
+                    "is_host": false
+                },
+                "net": {
+                    "inum": 4026533163,
+                    "is_host": false
+                },
+                "time": {
+                    "inum": 4026531834,
+                    "is_host": true
+                },
+                "time_for_children": {
+                    "inum": 4026531834,
+                    "is_host": true
+                },
+                "cgroup": {
+                    "inum": 4026533807,
+                    "is_host": false
+                },
+                "user": {
+                    "inum": 4026531837,
+                    "is_host": true
+                }
+            },
+            "tid": 2745117,
+            "process_credentials": {
+                "uid": 0,
+                "gid": 0,
+                "euid": 0,
+                "egid": 0,
+                "suid": 0,
+                "sgid": 0,
+                "fsuid": 0,
+                "fsgid": 0,
+                "securebits": [],
+                "caps": null,
+                "user_ns": null
+            },
+            "binary_properties": null,
+            "user": null,
+            "in_init_tree": false
+        },
+        "parent": {
+            "exec_id": "dnNoLWs4cy5hcHBzZWMtc3RhbmQucHRzZWN1cml0eS5jbG91ZDoxODEwNzg5OTk2NDM0Nzg5MDoyNzAxOTgy",
+            "pid": 2701982,
+            "uid": 0,
+            "cwd": "/",
+            "binary": "/usr/bin/bash",
+            "arguments": "",
+            "flags": "execve rootcwd",
+            "start_time": "2025-12-10T18:29:58.294617147Z",
+            "auid": 4294967295,
+            "pod": {
+                "namespace": "default",
+                "name": "test-pod-debian",
+                "container": {
+                    "id": "containerd://35ae39802175a6d328218c2d35d878bf0402b37a229ee28b2960161c567eca5d",
+                    "name": "test-pod-debian",
+                    "image": {
+                        "id": "docker.io/library/debian@sha256:7e5bc0e499a8d50cb1e32287944a90b9ec8fd7d500673e75daff3f52882f5798",
+                        "name": "docker.io/library/debian:12"
+                    },
+                    "start_time": "2025-12-10T07:50:30Z",
+                    "pid": 82,
+                    "maybe_exec_probe": false,
+                    "security_context": {
+                        "privileged": false
+                    }
+                },
+                "pod_labels": {},
+                "workload": "test-pod-debian",
+                "workload_kind": "Pod",
+                "pod_annotations": {}
+            },
+            "docker": "35ae39802175a6d328218c2d35d878b",
+            "parent_exec_id": "dnNoLWs4cy5hcHBzZWMtc3RhbmQucHRzZWN1cml0eS5jbG91ZDoxODEwNzg5OTk2MzIwNzExMDoyNzAxOTgy",
+            "refcnt": 0,
+            "cap": {
+                "permitted": [
+                    "CAP_CHOWN",
+                    "DAC_OVERRIDE",
+                    "CAP_FOWNER",
+                    "CAP_FSETID",
+                    "CAP_KILL",
+                    "CAP_SETGID",
+                    "CAP_SETUID",
+                    "CAP_SETPCAP",
+                    "CAP_NET_BIND_SERVICE",
+                    "CAP_NET_RAW",
+                    "CAP_SYS_CHROOT",
+                    "CAP_MKNOD",
+                    "CAP_AUDIT_WRITE",
+                    "CAP_SETFCAP"
+                ],
+                "effective": [
+                    "CAP_CHOWN",
+                    "DAC_OVERRIDE",
+                    "CAP_FOWNER",
+                    "CAP_FSETID",
+                    "CAP_KILL",
+                    "CAP_SETGID",
+                    "CAP_SETUID",
+                    "CAP_SETPCAP",
+                    "CAP_NET_BIND_SERVICE",
+                    "CAP_NET_RAW",
+                    "CAP_SYS_CHROOT",
+                    "CAP_MKNOD",
+                    "CAP_AUDIT_WRITE",
+                    "CAP_SETFCAP"
+                ],
+                "inheritable": []
+            },
+            "ns": {
+                "uts": {
+                    "inum": 4026533802,
+                    "is_host": false
+                },
+                "ipc": {
+                    "inum": 4026533803,
+                    "is_host": false
+                },
+                "mnt": {
+                    "inum": 4026533805,
+                    "is_host": false
+                },
+                "pid": {
+                    "inum": 4026533806,
+                    "is_host": false
+                },
+                "pid_for_children": {
+                    "inum": 4026533806,
+                    "is_host": false
+                },
+                "net": {
+                    "inum": 4026533163,
+                    "is_host": false
+                },
+                "time": {
+                    "inum": 4026531834,
+                    "is_host": true
+                },
+                "time_for_children": {
+                    "inum": 4026531834,
+                    "is_host": true
+                },
+                "cgroup": {
+                    "inum": 4026533807,
+                    "is_host": false
+                },
+                "user": {
+                    "inum": 4026531837,
+                    "is_host": true
+                }
+            },
+            "tid": 2701982,
+            "process_credentials": {
+                "uid": 0,
+                "gid": 0,
+                "euid": 0,
+                "egid": 0,
+                "suid": 0,
+                "sgid": 0,
+                "fsuid": 0,
+                "fsgid": 0,
+                "securebits": [],
+                "caps": null,
+                "user_ns": null
+            },
+            "binary_properties": null,
+            "user": null,
+            "in_init_tree": false
+        },
+        "function_name": "security_path_chmod",
+        "args": [
+            {
+                "path_arg": {
+                    "mount": "",
+                    "path": "/tmp/reg-file",
+                    "flags": "",
+                    "permission": "-rw-r--rw-"
+                },
+                "label": "file path"
+            },
+            {
+                "uint_arg": 423,
+                "label": "file mode"
+            }
+        ],
+        "return": null,
+        "action": "KPROBE_ACTION_POST",
+        "kernel_stack_trace": [],
+        "policy_name": "permissions-manipulation",
+        "return_action": "KPROBE_ACTION_POST",
+        "message": "",
+        "tags": [],
+        "user_stack_trace": [],
+        "ancestors": []
+    },
+    "node_name": "experts-k8s-cs",
+    "time": "2025-12-10T19:03:39.717614198Z",
+    "aggregation_info": null,
+    "cluster_name": "",
+    "node_labels": {
+        "beta.kubernetes.io/arch": "amd64",
+        "beta.kubernetes.io/os": "linux",
+        "kubernetes.io/arch": "amd64",
+        "kubernetes.io/hostname": "experts-k8s-cs",
+        "kubernetes.io/os": "linux",
+        "node-role.kubernetes.io/control-plane": "",
+        "node.kubernetes.io/exclude-from-external-load-balancers": ""
+    }
+}
+
 */
