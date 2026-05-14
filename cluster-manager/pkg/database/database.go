3af35d6f6c3d9f43508146172df82875b95ba1b7
Alexey Olshanskiy <234377865+avlllo@users.noreply.github.com>
2026-05-14 22:24:49 +0300

    feat: update cluster-manager to 0.2

--- cluster-manager/pkg/database/database.go
+++ cluster-manager/pkg/database/database.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/rs/zerolog/log"
 	"github.com/runtime-radar/runtime-radar/cluster-manager/pkg/model"
+	"github.com/runtime-radar/runtime-radar/lib/logger"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	gorm_logger "gorm.io/gorm/logger"
@@ -36,7 +37,7 @@ func New(address, database, user, password string, sslMode, sslCheckCert bool) (
 
 	if e := log.Debug(); e.Enabled() {
 		gormLogger = gorm_logger.New(
-			&GORMLogger{&log.Logger},
+			&logger.GORM{&log.Logger},
 			gorm_logger.Config{
 				SlowThreshold: 100 * time.Millisecond, // Slow SQL threshold
 				Colorful:      false,                  // Disable color
@@ -45,7 +46,7 @@ func New(address, database, user, password string, sslMode, sslCheckCert bool) (
 		)
 	} else {
 		gormLogger = gorm_logger.New(
-			&GORMLogger{&log.Logger},
+			&logger.GORM{&log.Logger},
 			gorm_logger.Config{
 				SlowThreshold: 100 * time.Millisecond, // Slow SQL threshold
 				Colorful:      false,                  // Disable color
