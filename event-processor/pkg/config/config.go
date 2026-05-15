1033f82acc4e5035868af814530896abfd4c93e6
Alexey Olshanskiy <234377865+avlllo@users.noreply.github.com>
2026-05-15 13:46:19 +0300

    feat: update event-processor to 0.2

--- event-processor/pkg/config/config.go
+++ event-processor/pkg/config/config.go
@@ -19,7 +19,7 @@ type Config struct {
 	LogLevel                              string        // log level can be INFO, WARN, ERROR, FATAL, DEBUG or ALL
 	LogFile                               string        // path to log file
 	ListenGRPCAddr                        string        // address "[host]:port" that server should be listening on
-	ListenHTTPAddr                        string        // address "[host]:port" that server should be listening for health checks
+	ListenHTTPAddr                        string        // address "[host]:port" that server should be listening on
 	InstrumentationAddr                   string        // address "[host]:port" that instrumentation server should be listening for health checks and metrics
 	TLS                                   bool          // is TLS enabled?
 	TokenKey                              string        // key for jwt token
@@ -37,6 +37,7 @@ type Config struct {
 	DeployDir                             string        // directory to load detectors from
 	PolicyEnforcerGRPCAddr                string        // Policy Enforcer address in host[:port] format
 	NotifierGRPCAddr                      string        // Notifier address in host[:port] format
+	OwnCSURL                              string        // URL of current CS (http(s)://host[:port]).
 }
 
 // New reads config from environment and returns pointer to a new Config.
@@ -70,6 +71,7 @@ func New() *Config {
 	flag.StringVar(&c.DeployDir, "deployDir", config.LookupEnvString("DEPLOY_DIR", "deploy"), "Set directory to load detectors from (doesn't apply if DB is not empty).")
 	flag.StringVar(&c.PolicyEnforcerGRPCAddr, "policyEnforcerGRPCAddr", config.LookupEnvString("POLICY_ENFORCER_GRPC_ADDR", "127.0.0.1:10000"), "Policy Enforcer gRPC address in host[:port] format.")
 	flag.StringVar(&c.NotifierGRPCAddr, "notifierGRPCAddr", config.LookupEnvString("NOTIFIER_GRPC_ADDR", "127.0.0.1:11000"), "Notifier gRPC address in host[:port] format.")
+	flag.StringVar(&c.OwnCSURL, "ownCSURL", config.LookupEnvString("OWN_CS_URL", ""), "URL of current CS (http(s)://host[:port]).")
 	flag.StringVar(&c.InstrumentationAddr, "listenInstrumentationAddr", config.LookupEnvString("LISTEN_INSTRUMENTATION_ADDR", ":9090"), `Address in form of "[host]:port" that instrumentation HTTP server should be listening on.`)
 
 	flag.Parse()
---
+++ event-processor/pkg/processor/detector/detector/detector.go
@@ -0,0 +1,81 @@
+//go:build !tinygo.wasm
+
+package detector
+
+import (
+	"context"
+	"fmt"
+
+	tetragon_api "github.com/cilium/tetragon/api/v1/tetragon"
+	detector_api "github.com/runtime-radar/runtime-radar/event-processor/detector/api"
+	detector_tetragon_api "github.com/runtime-radar/runtime-radar/event-processor/detector/api/tetragon"
+	"github.com/runtime-radar/runtime-radar/event-processor/pkg/model"
+	"github.com/runtime-radar/runtime-radar/lib/security"
+	"github.com/tetratelabs/wazero"
+	"google.golang.org/protobuf/proto"
+)
+
+func NewPlugin(ctx context.Context) (*detector_api.DetectorPlugin, error) {
+	mc := wazero.NewModuleConfig()
+
+	p, err := detector_api.NewDetectorPlugin(ctx, detector_api.WazeroModuleConfig(mc))
+	if err != nil {
+		return nil, fmt.Errorf("can't instantiate plugin: %w", err)
+	}
+
+	return p, nil
+}
+
+func BinsToDetectors(ctx context.Context, plugin *detector_api.DetectorPlugin, bins [][]byte) ([]detector_api.Detector, error) {
+	ds := make([]detector_api.Detector, 0, len(bins))
+
+	for _, bin := range bins {
+		d, err := plugin.LoadBinary(ctx, bin)
+		if err != nil {
+			return nil, fmt.Errorf("can't load binary: %w", err)
+		}
+		ds = append(ds, d)
+	}
+
+	return ds, nil
+}
+
+func BinToModel(ctx context.Context, plugin *detector_api.DetectorPlugin, bin []byte) (*model.Detector, error) {
+	detector, err := plugin.LoadBinary(ctx, bin)
+	if err != nil {
+		return nil, fmt.Errorf("can't load wasm module: %w", err)
+	}
+
+	infoResp, err := detector.Info(ctx, &detector_api.InfoReq{})
+	if err != nil {
+		return nil, fmt.Errorf("can't get detector info: %w", err)
+	}
+
+	d := &model.Detector{
+		ID:          infoResp.GetId(),
+		Name:        infoResp.GetName(),
+		Description: infoResp.GetDescription(),
+		Version:     uint(infoResp.GetVersion()),
+		Author:      infoResp.GetAuthor(),
+		Contact:     infoResp.GetContact(),
+		License:     infoResp.GetLicense(),
+		WasmBinary:  bin,
+		WasmHash:    security.HashSHA512AsHex(bin),
+	}
+
+	return d, err
+}
+
+func convertEvent(in *tetragon_api.GetEventsResponse) (*detector_tetragon_api.GetEventsResponse, error) {
+	b, err := proto.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+
+	out := &detector_tetragon_api.GetEventsResponse{}
+	if err := out.UnmarshalVT(b); err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
