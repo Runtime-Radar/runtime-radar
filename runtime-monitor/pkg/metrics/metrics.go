3d0d7f828a7da22867ce74fb38c5a564cd172cfc
Alexey Olshanskiy <234377865+avlllo@users.noreply.github.com>
2026-05-19 17:36:35 +0300

    feat: update runtime-monitor to 0.2

---
+++ runtime-monitor/pkg/metrics/metrics.go
@@ -0,0 +1,70 @@
+package metrics
+
+import (
+	"strconv"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/runtime-radar/runtime-radar/lib/metrics"
+)
+
+const (
+	queueLabel      = "queue"
+	isConsumerLabel = "is_consumer"
+)
+
+var (
+	commonMetrics []prometheus.Collector
+
+	RabbitAddEventsSuccessCount = addToCommon(prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "rabbit_add_events_success_count",
+		Help: "Number of events successfully added to the rabbitmq queue",
+	}))
+	RabbitAddEventsFailureCount = addToCommon(prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "rabbit_add_events_failure_count",
+		Help: "Number of events that failed to be added to the rabbitmq queue",
+	}))
+	TetragonEventsBufferSizeGauge = addToCommon(prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "tetragon_events_buffer_size_gauge",
+		Help: "Number of events in the buffer received from the tetragon",
+	}))
+	TetragonEventsDroppedCount = addToCommon(prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "tetragon_events_dropped_count",
+		Help: "Number of tetragon events dropped",
+	}))
+	RabbitBrokerConnectionStateGauge = addToCommon(prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "rabbit_broker_connection_state_gauge",
+		Help: "Availability of broker connection to rabbit (1 - available, 0 - not available)",
+	}, []string{queueLabel, isConsumerLabel}))
+
+	// Example addToCommon definition:
+	//
+	//	ProxySuccessCount = addToCommon(prometheus.NewCounterVec(prometheus.CounterOpts{
+	//		Name: "proxy_success_count",
+	//		Help: "The number of successfully processed messages on proxy service",
+	//	}, []string{myLabel}))
+)
+
+// PrepareRegistry creates and registers service global metrics
+func PrepareRegistry(service, cluster string, m ...prometheus.Collector) (*metrics.Registry, error) {
+	var mm []prometheus.Collector
+	mm = append(mm, commonMetrics...)
+	mm = append(mm, m...)
+
+	return metrics.NewRegistry(service, cluster, mm...)
+}
+
+func RabbitStateReporter(queue string, isConsumer bool) func(bool) {
+	return func(isAlive bool) {
+		var value float64
+		if isAlive {
+			value = 1
+		}
+
+		RabbitBrokerConnectionStateGauge.With(prometheus.Labels{queueLabel: queue, isConsumerLabel: strconv.FormatBool(isConsumer)}).Set(value)
+	}
+}
+
+func addToCommon[T prometheus.Collector](metric T) T {
+	commonMetrics = append(commonMetrics, metric)
+	return metric
+}
