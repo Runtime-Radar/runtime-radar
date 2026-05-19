08242956ca520210655a3732b359195fa077b2cd
Alexey Olshanskiy <234377865+avlllo@users.noreply.github.com>
2026-05-19 17:20:14 +0300

    feat: update public-api to 0.2

--- public-api/pkg/server/middleware/middleware.go
+++ public-api/pkg/server/middleware/middleware.go
@@ -3,9 +3,14 @@ package middleware
 import (
 	"context"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/runtime-radar/runtime-radar/lib/server/interceptor"
+	"github.com/runtime-radar/runtime-radar/public-api/pkg/metrics"
 	"google.golang.org/grpc/metadata"
 )
 
@@ -59,4 +64,55 @@ func Correlation(next http.Handler) http.Handler {
 func AccessTokenFromContext(ctx context.Context) (string, bool) {
 	token, ok := ctx.Value(accessTokenContextKey).(string)
 	return token, ok
+}
+
+func Metrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/metrics" {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		start := time.Now()
+		sr := &statusRecorder{ResponseWriter: w, statusCode: "2xx"}
+		next.ServeHTTP(sr, r)
+
+		metrics.RequestCounter.With(
+			prometheus.Labels{
+				metrics.Method: r.Method,
+				metrics.Code:   sr.statusCode,
+				metrics.Path:   normalizePath(r.URL.Path),
+			},
+		).Add(1)
+
+		metrics.RequestDurationHistogram.Observe(time.Since(start).Seconds())
+	})
+}
+
+type statusRecorder struct {
+	http.ResponseWriter
+	statusCode string
+}
+
+func (rw *statusRecorder) WriteHeader(code int) {
+	c := strconv.Itoa(code)
+	rw.statusCode = string(c[0]) + "xx"
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func normalizePath(path string) string {
+	parts := strings.Split(path, "/")
+	for i, part := range parts {
+		if _, err := strconv.ParseUint(part, 10, 64); err == nil {
+			parts[i] = "{num}"
+
+			continue
+		}
+
+		if uuid.Validate(part) == nil {
+			parts[i] = "{uuid}"
+		}
+	}
+
+	return strings.Join(parts, "/")
 }
--- public-api/pkg/service/rule_generic.go
+++ public-api/pkg/service/rule_generic.go
@@ -1,9 +1,9 @@
 package service
 
 import (
-	enf_api "github.com/runtime-radar/runtime-radar/policy-enforcer/api"
+	enf_pb "github.com/runtime-radar/runtime-radar/policy-enforcer/api"
 )
 
 type RuleGeneric struct {
-	enf_api.RuleControllerClient
+	enf_pb.RuleControllerClient
 }
